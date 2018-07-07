68d93cc8d5bf3145a66604ebd139349995283645
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-07-06 21:06:49 -0300

    ci: fix travis publish.sh script.

--- travis/publish.sh
+++ travis/publish.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -ex
 
 version="latest"
-if [ $TRAVIS_BRANCH != "master" ] ; then
-  version=$TRAVIS_BRANCH
+if [ ${TRAVIS_BRANCH} != "master" ] ; then
+  version=${TRAVIS_BRANCH}
 fi
-
-tag=dojot/ejbca-rest:$version
+tag=${TRAVIS_REPO_SLUG}:$version
 
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
-docker tag dojot/ejbca-rest ${tag}
+docker tag ${TRAVIS_REPO_SLUG} ${tag}
 docker push $tag
